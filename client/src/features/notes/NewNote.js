43c21fa3ff985b6365c01577f20d10483049b8e9
haivuanh <haivuanh29031985@gmail.com>
2023-01-03 11:54:26 +0700

    Login auth

--- client/src/features/notes/NewNote.js
+++ client/src/features/notes/NewNote.js
@@ -4,8 +4,9 @@ import NewNoteForm from './NewNoteForm';
 
 const NewNote = () => {
   const users = useSelector(selectAllUsers);
+  if (!users?.length) return <p>Not Currently Available</p>;
 
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
+  const content = <NewNoteForm users={users} />;
 
   return content;
 };
